d895694d6f750ac59305a9d1ee4e03cd07a92f06
omjoonkim <omjoonkim@gmail.com>
2019-11-23 12:58:43 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "deprothon-plz-disturb-api"
---
+++ src/main/kotlin/com/depromeet/plzdisturb/deprothonplzdisturbapi/DeprothonPlzDisturbApiApplication.kt
@@ -0,0 +1,11 @@
+package com.depromeet.plzdisturb.deprothonplzdisturbapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeprothonPlzDisturbApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeprothonPlzDisturbApiApplication>(*args)
+}
